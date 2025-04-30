03716663848a4c9ecc7cc12b8fc3f8ccf3c9f126
Debanjan <debanjankanjilal9@gmail.com>
2025-04-30 14:52:11 +0530

    return anonymous function

--- httpd/main.go
+++ httpd/main.go
@@ -12,7 +12,7 @@ func main() {
 	r := gin.Default()
 
 	// defines the gateway "/ping" and prepares a response in form of "message" : "pong"
-	r.GET("/ping", handler.PingGet)
+	r.GET("/ping", handler.PingGet())
 
 	// runs the server
 	r.Run(":8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
