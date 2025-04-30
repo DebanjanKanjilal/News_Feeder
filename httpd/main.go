c40b5a624946f9e1e6c046f38da277b0e2456e9c
Debanjan <debanjankanjilal9@gmail.com>
2025-04-30 14:20:32 +0530

    customized and optimized the function

--- httpd/main.go
+++ httpd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"net/http"
+	"news-feeder/httpd/handler"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,11 +12,7 @@ func main() {
 	r := gin.Default()
 
 	// defines the gateway "/ping" and prepares a response in form of "message" : "pong"
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping", handler.PingGet)
 
 	// runs the server
 	r.Run(":8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
